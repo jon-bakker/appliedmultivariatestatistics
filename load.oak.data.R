2591098771050771815ac57096e54ff59cdf8679
Jon Bakker <jon-bakker@users.noreply.github.com>
2026-01-22 16:47:56 -0800

    Script to load and pre-process oak data

--- load.oak.data.R
+++ load.oak.data.R
@@ -1,15 +1,13 @@
 ## Script to load and conduct initial adjustments to oak plant community dataset
 ## JD Bakker
-## 190123
+## 260122
+
+# Assumes that you have opened the R project that is saved in your SEFS 502 folder.
 
 # Load packages
 require(vegan)
 require(labdsv)
-require(magrittr)
-
-# Set working directory
-setwd(choose.dir())
-#setwd("C://Users/jbakker/Desktop/SEFS 502/")
+require(tidyverse)
 
 # Load data
 Oak <- read.csv("data/Oak_data_47x216.csv", header = TRUE, row.names = 1) 
@@ -20,6 +18,9 @@ Oak_abund <- Oak[ , colnames(Oak) %in% Oak_spp$SpeciesCode]
 Oak_explan <- Oak[ , ! colnames(Oak) %in% Oak_spp$SpeciesCode]
 
 # Make adjustments to response variables
-Oak1 <- Oak_abund %>%
-  vegtab(minval = 0.05 * nrow(Oak_abund)) %>% # Remove rare species
+Oak1 <- Oak_abund |>
+  vegtab(minval = 0.05 * nrow(Oak_abund)) |> # Remove rare species
   decostand("max") # Relativize by species maxima
+
+Oak1.dist <- vegdist(Oak1)
+
