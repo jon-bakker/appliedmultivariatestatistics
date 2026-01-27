1b83369ad21a43ff162c0d724f2062bbebde9059
Jon Bakker <jon-bakker@users.noreply.github.com>
2026-01-26 16:05:39 -0800

    function to conduct pairwise comparisons via adonis2()

--- pairwise.adonis2.R
+++ pairwise.adonis2.R
@@ -6,8 +6,7 @@ pairwise.adonis2 <- function(resp, fact, p.method = "none", nperm = 999) {
     fact2 <- droplevels(fact[as.numeric(fact) %in% c(i, j)])
     index <- which(fact %in% levels(fact2))
     resp2 <- as.dist(resp[index, index])
-    result <- adonis2(resp2 ~ fact2, permutations = nperm)
-    result$`Pr(>F)`[1]
+    result <- adonis2(resp2 ~ fact2, permutations = nperm, by = "terms")[1, "Pr(>F)"]
   }
   multcomp <- pairwise.table(fun.p, levels(fact), p.adjust.method = p.method)
   return(list(fact = levels(fact), p.value = multcomp, p.adjust.method = p.method))
